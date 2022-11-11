7adf47b42ca79d4d58ee6079b3bf4e5055c4aa38
DaveSharma-Hub <davesharma12345@outlook.com>
2022-11-11 11:58:29 -0700

    file upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function objectToArray(objectValue){
+    return objectValue ? Object.entries(objectValue) : [];
+}
+
+function objectArrayToArray(objectArray){
+    let array = []
+    if(!objectArray) return array;
+    objectArray.map((item)=>{
+        array.push(objectToArray(item));
+    })
+    return array;
+}
+
+function splitString(stringValue,delimeter){
+    let array=[];
+    let tmp = [];
+    for(let i=0;i<stringValue.length;i=i+1){
+        if(stringValue[i]===delimeter){
+            array.push(tmp);
+            tmp = [];
+        }else{
+            tmp.push(stringValue[i]);
+        }
+    }
+    array.push(tmp);
+    return array;
+}
